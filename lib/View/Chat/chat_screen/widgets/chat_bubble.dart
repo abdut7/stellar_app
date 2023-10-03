aee578a1e24a8a85b38a0ad72b190c0d72ba4a1b
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-10-03 16:27:45 +0530

    Sent message personal chat UI fixed

--- lib/View/Chat/chat_screen/widgets/chat_bubble.dart
+++ lib/View/Chat/chat_screen/widgets/chat_bubble.dart
@@ -24,120 +24,187 @@ class ChatBubble extends StatelessWidget {
         isSent ? CrossAxisAlignment.end : CrossAxisAlignment.start;
     final color = isSent ? const Color.fromRGBO(233, 244, 255, 1) : Colors.grey;
 
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: alignment,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: message.strMessageType == "voice" ? null : color,
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-            ),
-            padding: const EdgeInsets.all(2),
-            child: message.strMessageType == "text"
-                ? Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 24, vertical: 12),
-                    child: Container(
-                      width: Get.width * 0.4,
-                      child: Text(
-                        message.strMessage,
-                        style: const TextStyle(
-                            color: Color.fromRGBO(87, 87, 87, 1)),
-                      ),
-                    ),
-                  )
-                : message.strMessageType == "image"
-                    ? GestureDetector(
-                        onTap: () {
-                          Get.to(
-                              () => PhotoViewScreen(imageUrl: message.strUrl));
-                        },
-                        child: SizedBox(
-                            height: Get.width * 0.4,
-                            width: Get.width * 0.6,
-                            child: Padding(
-                              padding: message.strMessageType == "text"
-                                  ? const EdgeInsets.all(8)
-                                  : message.strMessageType == "image"
-                                      ? const EdgeInsets.all(2.0)
-                                      : const EdgeInsets.all(0),
-                              child: CachedNetworkImage(
-                                fit: BoxFit.cover,
-                                imageUrl: message.strUrl,
-                                progressIndicatorBuilder:
-                                    (context, url, downloadProgress) => Center(
-                                  child: CircularProgressIndicator(
-                                      value: downloadProgress.progress),
-                                ),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          // width: Get.width * 0.5,
+          margin: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: alignment,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  color: message.strMessageType == "voice" ? null : color,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft:
+                          isSent ? Radius.circular(10) : Radius.circular(0),
+                      bottomRight:
+                          isSent ? Radius.circular(0) : Radius.circular(10)),
+                ),
+                padding: const EdgeInsets.all(2),
+                child: message.strMessageType == "text"
+                    ? Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 12),
+                            child: Container(
+                              width: Get.width * 0.4,
+                              child: Text(
+                                message.strMessage,
+                                style: const TextStyle(
+                                    color: Color.fromRGBO(87, 87, 87, 1)),
                               ),
-                            )),
-                      )
-                    : message.strMessageType == "voice"
-                        ? AudioMessageBubble(
-                            createdTime: message.strCreatedTime,
-                            audioUrl: message.strUrl,
-                            isSender: message.strUserId ==
-                                controller.userDetailsModel.value!.id,
-                          )
-                        : message.strMessageType == "sentingImage"
-                            ? Padding(
-                                padding: const EdgeInsets.all(2.0),
-                                child: Container(
-                                  height: Get.width * 0.4,
-                                  width: Get.width * 0.6,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          fit: BoxFit.cover,
-                                          image:
-                                              FileImage(File(message.strUrl)))),
-                                  child: const Center(
-                                    child: CircularProgressIndicator(),
+                            ),
+                          ),
+                          Container(
+                            height: 15,
+                            width: Get.width * 0.6,
+                            decoration: const BoxDecoration(
+                                color: Color.fromRGBO(197, 229, 255, 1),
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(10))),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      bottom: 2.0, right: 5),
+                                  child: Text(
+                                    message.strCreatedTime,
+                                    style: const TextStyle(
+                                        color: Colors.grey, fontSize: 10),
                                   ),
                                 ),
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                    : message.strMessageType == "image"
+                        ? GestureDetector(
+                            onTap: () {
+                              Get.to(() =>
+                                  PhotoViewScreen(imageUrl: message.strUrl));
+                            },
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                    height: Get.width * 0.4,
+                                    width: Get.width * 0.6,
+                                    child: Padding(
+                                      padding: message.strMessageType == "text"
+                                          ? const EdgeInsets.all(8)
+                                          : message.strMessageType == "image"
+                                              ? const EdgeInsets.all(0)
+                                              : const EdgeInsets.all(0),
+                                      child: CachedNetworkImage(
+                                        fit: BoxFit.cover,
+                                        imageUrl: message.strUrl,
+                                        progressIndicatorBuilder:
+                                            (context, url, downloadProgress) =>
+                                                Center(
+                                          child: CircularProgressIndicator(
+                                              value: downloadProgress.progress),
+                                        ),
+                                        errorWidget: (context, url, error) =>
+                                            const Icon(Icons.error),
+                                      ),
+                                    )),
+                                Container(
+                                  height: 15,
+                                  width: Get.width * 0.6,
+                                  decoration: const BoxDecoration(
+                                      color: Color.fromRGBO(197, 229, 255, 1),
+                                      borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(10))),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            bottom: 2.0, right: 5),
+                                        child: Text(
+                                          message.strCreatedTime,
+                                          style: const TextStyle(
+                                              color: Colors.grey, fontSize: 10),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        : message.strMessageType == "voice"
+                            ? AudioMessageBubble(
+                                createdTime: message.strCreatedTime,
+                                audioUrl: message.strUrl,
+                                isSender: isSent,
                               )
-                            : const SizedBox(),
+                            : message.strMessageType == "sentingImage"
+                                ? Padding(
+                                    padding: const EdgeInsets.all(2.0),
+                                    child: Container(
+                                      height: Get.width * 0.4,
+                                      width: Get.width * 0.6,
+                                      decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                              fit: BoxFit.cover,
+                                              image: FileImage(
+                                                  File(message.strUrl)))),
+                                      child: const Center(
+                                        child: CircularProgressIndicator(),
+                                      ),
+                                    ),
+                                  )
+                                : const SizedBox(),
+              ),
+              // const SizedBox(height: 4.0),
+            ],
           ),
-          // const SizedBox(height: 4.0),
-          message.strMessageType != "sentingImage"
-              ? Container(
-                  decoration: BoxDecoration(
-                      color: isSent
-                          ? const Color.fromRGBO(197, 229, 255, 1)
-                          : const Color.fromRGBO(224, 224, 224, 1),
-                      borderRadius: const BorderRadius.vertical(
-                          bottom: Radius.circular(10))),
-                  width: message.strUrl.isEmpty
-                      ? Get.width * 0.5 + 2
-                      : Get.width * 0.6,
-                  child: Row(
-                    mainAxisAlignment: isSent
-                        ? MainAxisAlignment.end
-                        : MainAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 2.0),
-                        child: Text(
-                          message.strCreatedTime,
-                          style: const TextStyle(color: Colors.grey),
-                        ),
-                      ),
-                      const SizedBox(width: 4.0),
-                      Icon(
-                        isSent ? Icons.done_all : Icons.done,
-                        color: Colors.grey,
-                        size: 16.0,
-                      ),
-                    ],
-                  ),
-                )
-              : const SizedBox(),
-        ],
-      ),
+        ),
+        // (message.strMessageType != "sentingImage" &&
+        //         message.strMessageType != "voice")
+        //     ? Container(
+        //         width: Get.width * 0.5,
+        //         decoration: BoxDecoration(
+        //             color: isSent
+        //                 ? const Color.fromRGBO(197, 229, 255, 1)
+        //                 : const Color.fromRGBO(224, 224, 224, 1),
+        //             borderRadius: const BorderRadius.vertical(
+        //                 bottom: Radius.circular(10))),
+        //         // width: message.strUrl.isEmpty
+        //         //     ? Get.width * 0.5 + 2
+        //         //     : Get.width * 0.6,
+        //         child: Row(
+        //           mainAxisAlignment:
+        //               isSent ? MainAxisAlignment.end : MainAxisAlignment.start,
+        //           children: <Widget>[
+        //             Padding(
+        //               padding: const EdgeInsets.only(bottom: 2.0),
+        //               child: Text(
+        //                 message.strCreatedTime,
+        //                 style: const TextStyle(color: Colors.grey),
+        //               ),
+        //             ),
+        //             const SizedBox(width: 4.0),
+        //             Icon(
+        //               isSent ? Icons.done_all : Icons.done,
+        //               color: Colors.grey,
+        //               size: 16.0,
+        //             ),
+        //           ],
+        //         ),
+        //       )
+        //     : const SizedBox(),
+        const SizedBox(
+          height: 10,
+        )
+      ],
     );
   }
 }
--- lib/View/Chat/group_chat/widgets/voice_chat_bubble.dart
+++ lib/View/Chat/group_chat/widgets/voice_chat_bubble.dart
@@ -70,98 +70,121 @@ class _AudioMessageBubbleState extends State<AudioMessageBubble> {
 
     return Align(
       alignment: widget.isSender ? Alignment.topRight : Alignment.topLeft,
-      child: Container(
-        width: Get.width * 0.6,
-        margin: const EdgeInsets.only(
-          top: 4.0,
-        ),
-        decoration: BoxDecoration(
-          color: widget.isSender
-              ? const Color.fromRGBO(233, 244, 255, 1)
-              : const Color.fromRGBO(244, 244, 244, 1),
-          borderRadius: borderRadius,
-        ),
-        child: Column(
-          crossAxisAlignment: widget.isSender
-              ? CrossAxisAlignment.end
-              : CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
+      child: Column(
+        children: [
+          Container(
+            width: Get.width * 0.6,
+            margin: const EdgeInsets.only(
+              top: 4.0,
+            ),
+            decoration: BoxDecoration(
+              color: widget.isSender
+                  ? const Color.fromRGBO(233, 244, 255, 1)
+                  : const Color.fromRGBO(244, 244, 244, 1),
+              borderRadius: borderRadius,
+            ),
+            child: Column(
+              crossAxisAlignment: widget.isSender
+                  ? CrossAxisAlignment.end
+                  : CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    backgroundColor: Color.fromRGBO(0, 51, 142, 1),
-                    child: Icon(
-                      Icons.headphones,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(
-                    audioPlayerState == PlayerState.playing
-                        ? Icons.pause
-                        : Icons.play_arrow,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    if (audioPlayerState == PlayerState.playing) {
-                      audioPlayer.pause();
-                    } else {
-                      audioPlayer.play(UrlSource(widget.audioUrl));
-                    }
-                  },
-                ),
-                Expanded(
-                  child: GestureDetector(
-                    onHorizontalDragUpdate: (details) {
-                      // Handle swipe gestures for fast-forwarding
-                      final double screenWidth =
-                          MediaQuery.of(context).size.width;
-                      final double dragDistance = details.primaryDelta ?? 0.0;
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 8, top: 8, bottom: 8),
+                        child: CircleAvatar(
+                          backgroundColor: Color.fromRGBO(0, 51, 142, 1),
+                          child: Icon(
+                            Icons.headphones,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          audioPlayerState == PlayerState.playing
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          if (audioPlayerState == PlayerState.playing) {
+                            audioPlayer.pause();
+                          } else {
+                            audioPlayer.play(UrlSource(widget.audioUrl));
+                          }
+                        },
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onHorizontalDragUpdate: (details) {
+                            // Handle swipe gestures for fast-forwarding
+                            final double screenWidth =
+                                MediaQuery.of(context).size.width;
+                            final double dragDistance =
+                                details.primaryDelta ?? 0.0;
 
-                      setState(() {
-                        final double newSliderValue =
-                            (sliderValue + (dragDistance / screenWidth))
-                                .clamp(0.0, 1.0);
-                        sliderValue = newSliderValue;
-                        final double newPosition =
-                            newSliderValue * totalDuration.inMilliseconds;
-                        audioPlayer
-                            .seek(Duration(milliseconds: newPosition.round()));
-                      });
-                    },
-                    child: LinearProgressIndicator(
-                      value: sliderValue,
-                      valueColor: const AlwaysStoppedAnimation<Color>(
-                          Color.fromRGBO(0, 51, 142, 1)),
-                      backgroundColor: Colors.grey[400],
-                    ),
+                            setState(() {
+                              final double newSliderValue =
+                                  (sliderValue + (dragDistance / screenWidth))
+                                      .clamp(0.0, 1.0);
+                              sliderValue = newSliderValue;
+                              final double newPosition =
+                                  newSliderValue * totalDuration.inMilliseconds;
+                              audioPlayer.seek(
+                                  Duration(milliseconds: newPosition.round()));
+                            });
+                          },
+                          child: LinearProgressIndicator(
+                            value: sliderValue,
+                            valueColor: const AlwaysStoppedAnimation<Color>(
+                                Color.fromRGBO(0, 51, 142, 1)),
+                            backgroundColor: Colors.grey[400],
+                          ),
+                        ),
+                      ),
+                      // const SizedBox(
+                      //   width: 8,
+                      // ),
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  width: 8,
+                // Text(
+                //   '${completedDuration.inMinutes}:${(completedDuration.inSeconds % 60).toString().padLeft(2, '0')} / ${totalDuration.inMinutes}:${(totalDuration.inSeconds % 60).toString().padLeft(2, '0')}',
+                //   style: const TextStyle(color: Colors.black),
+                // ),
+                // Text(
+                //   widget.createdTime,
+                //   style: const TextStyle(color: Colors.grey, fontSize: 8),
+                // ),
+              ],
+            ),
+          ),
+          Container(
+            height: 15,
+            width: Get.width * 0.6,
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(197, 229, 255, 1),
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(10))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 2.0, right: 5),
+                  child: Text(
+                    widget.createdTime,
+                    style: const TextStyle(color: Colors.grey, fontSize: 10),
+                  ),
                 ),
               ],
             ),
-            Text(
-              '${completedDuration.inMinutes}:${(completedDuration.inSeconds % 60).toString().padLeft(2, '0')} / ${totalDuration.inMinutes}:${(totalDuration.inSeconds % 60).toString().padLeft(2, '0')}',
-              style: const TextStyle(color: Colors.black),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Text(
-              widget.createdTime,
-              style: const TextStyle(color: Colors.grey, fontSize: 8),
-            ),
-            SizedBox(
-              height: 6,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
