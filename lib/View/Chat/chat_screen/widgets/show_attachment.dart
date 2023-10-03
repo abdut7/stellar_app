7b19a0ee49fb18a6f7811ec99eb3752b35f03b79
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-10-03 23:23:39 +0530

    Attach file UI changes done

--- lib/View/Chat/chat_screen/widgets/show_attachment.dart
+++ lib/View/Chat/chat_screen/widgets/show_attachment.dart
@@ -18,16 +18,21 @@ Future<dynamic> show_attachment(
     context: context,
     builder: (context) {
       return Padding(
-        padding: EdgeInsets.only(bottom: Get.height * 0.1, left: 12, right: 12),
-        child: Container(
-            height: 230,
+          padding:
+              EdgeInsets.only(bottom: Get.height * 0.1, left: 12, right: 12),
+          child: Container(
             width: Get.width * 0.9,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color.fromRGBO(159, 196, 232, 1)),
+              borderRadius: BorderRadius.circular(10),
+              color: const Color.fromRGBO(159, 196, 232, 1),
+            ),
             child: GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisSpacing: 2,
+                crossAxisSpacing: 1,
+                childAspectRatio: 1,
+                mainAxisSpacing: 1,
                 crossAxisCount: 3, // 3 elements in each row
               ),
               itemBuilder: (BuildContext context, int index) {
@@ -36,23 +41,27 @@ Future<dynamic> show_attachment(
                   onTap: () {
                     onTap(index);
                   },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      children: [
-                        SvgPicture.asset(attachList[index]['file']!),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text(attachList[index]['name']!)
-                      ],
+                  child: Container(
+                    width: Get.width / 4,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SvgPicture.asset(attachList[index]['file']!),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(attachList[index]['name']!)
+                        ],
+                      ),
                     ),
                   ),
                 );
               },
               itemCount: 6, // Total number of elements
-            )),
-      );
+            ),
+          ));
     },
   );
 }
