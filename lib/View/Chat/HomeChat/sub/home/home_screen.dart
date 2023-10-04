ee37205497c5da68b66cab4bfa53a919f4023361
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-10-04 16:46:30 +0530

    given a fix for refresh in chat history

--- lib/View/Chat/HomeChat/sub/home/home_screen.dart
+++ lib/View/Chat/HomeChat/sub/home/home_screen.dart
@@ -40,86 +40,88 @@ class _HomeScreenState extends State<HomeScreen> {
     ChatHistoryController chatHistoryController = Get.put(
       ChatHistoryController(),
     );
-    return Scaffold(
-      backgroundColor: SColors.color4,
-      appBar: AppBar(
-        backgroundColor: SColors.color12,
-        toolbarHeight: 0,
-        elevation: 0,
-      ),
-      body: RefreshIndicator(
-        onRefresh: refreshChatHistory,
-        child: Column(
-          children: [
-            const ChatHomeAppbarWidget(),
-            // const SizedBox(height: 8),
-            const SearchTextField(),
-            // const SizedBox(height: 8),
-            Obx(() {
-              if (chatHistoryController.isLoading.value) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (chatHistoryController.errorOccured.value) {
-                return const Center(
-                  child: Text("Error occured while loading chat"),
-                );
-              } else if (chatHistoryController.chatHistoryList.isEmpty) {
-                return const Center(
-                  child: Text("No Chat Exist"),
-                );
-              }
-
-              return Expanded(
-                child: ListView.separated(
-                  padding: EdgeInsets.zero,
-                  itemBuilder: (context, index) {
-                    ChatHistoryList data =
-                        chatHistoryController.chatHistoryList[index];
-                    return GestureDetector(
-                      onTap: () {},
-                      child: ChatListItem(
-                        chatId: data.id,
-                        avatarUrl: data.strIconURL,
-                        message: data.strMessage,
-                        name: data.strName.isEmpty ? "Unknown" : data.strName,
-                        time: data.strCreatedTime,
-                        unreadCount: 0,
-                        ontap: () {
-                          if (data.strType == "private") {
-                            Get.to(() => PrivateChatScreen(
-                                imageUrl: data.strIconURL,
-                                fullName: data.strName,
-                                chatId: data.strChatId));
-                          } else if (data.strType == "group") {
-                            Get.to(() => GroupChatScreen(
-                                  chatHistoryList: data,
-                                ));
-                          }
-                        },
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => const Divider(),
-                  itemCount: chatHistoryController.chatHistoryList.length,
-                ),
-              );
-            })
-          ],
+    return RefreshIndicator(
+      onRefresh: refreshChatHistory,
+      child: Scaffold(
+        backgroundColor: SColors.color4,
+        appBar: AppBar(
+          backgroundColor: SColors.color12,
+          toolbarHeight: 0,
+          elevation: 0,
         ),
-      ),
-      floatingActionButton: InkWell(
-        onTap: () {
-          Get.to(() => const ShowContactsScreen());
-        },
-        child: Container(
-          width: 70,
-          height: 70,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: SColors.color13,
-            image: const DecorationImage(
-              image: AssetImage('assets/Images/all_contacts_icon.png'),
+        body: Container(
+          child: Column(
+            children: [
+              const ChatHomeAppbarWidget(),
+              // const SizedBox(height: 8),
+              const SearchTextField(),
+              // const SizedBox(height: 8),
+              Obx(() {
+                if (chatHistoryController.isLoading.value) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (chatHistoryController.errorOccured.value) {
+                  return const Center(
+                    child: Text("Error occured while loading chat"),
+                  );
+                } else if (chatHistoryController.chatHistoryList.isEmpty) {
+                  return const Center(
+                    child: Text("No Chat Exist"),
+                  );
+                }
+
+                return Expanded(
+                  child: ListView.separated(
+                    padding: EdgeInsets.zero,
+                    itemBuilder: (context, index) {
+                      ChatHistoryList data =
+                          chatHistoryController.chatHistoryList[index];
+                      return GestureDetector(
+                        onTap: () {},
+                        child: ChatListItem(
+                          chatId: data.id,
+                          avatarUrl: data.strIconURL,
+                          message: data.strMessage,
+                          name: data.strName.isEmpty ? "Unknown" : data.strName,
+                          time: data.strCreatedTime,
+                          unreadCount: 0,
+                          ontap: () {
+                            if (data.strType == "private") {
+                              Get.to(() => PrivateChatScreen(
+                                  imageUrl: data.strIconURL,
+                                  fullName: data.strName,
+                                  chatId: data.strChatId));
+                            } else if (data.strType == "group") {
+                              Get.to(() => GroupChatScreen(
+                                    chatHistoryList: data,
+                                  ));
+                            }
+                          },
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) => const Divider(),
+                    itemCount: chatHistoryController.chatHistoryList.length,
+                  ),
+                );
+              })
+            ],
+          ),
+        ),
+        floatingActionButton: InkWell(
+          onTap: () {
+            Get.to(() => const ShowContactsScreen());
+          },
+          child: Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: SColors.color13,
+              image: const DecorationImage(
+                image: AssetImage('assets/Images/all_contacts_icon.png'),
+              ),
             ),
           ),
         ),
