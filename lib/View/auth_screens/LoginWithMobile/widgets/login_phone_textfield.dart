b4268fbcab93a80401f1c300124a338620b24844
Nithin-M-007 <isnithinm2001@gmail.com>
2023-10-08 11:03:47 +0530

    login with mobile screen text field issue during validation  solved

--- lib/View/auth_screens/LoginWithMobile/widgets/login_phone_textfield.dart
+++ lib/View/auth_screens/LoginWithMobile/widgets/login_phone_textfield.dart
@@ -1,5 +1,159 @@
-import 'package:fl_country_code_picker/fl_country_code_picker.dart';
+// import 'package:fl_country_code_picker/fl_country_code_picker.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:stellar_chat/Settings/SColors.dart';
+//
+// CountryCode? publiccountryCode;
+//
+// class LoginPhoneTextField extends StatefulWidget {
+//   final TextInputType keyboardType;
+//   final String labelText;
+//   final TextEditingController controller;
+//   final String? Function(String? val)? validator;
+//   final Function(String? val)? onSaved;
+//   final Function(String val)? onChanged;
+//
+//   const LoginPhoneTextField({
+//     Key? key,
+//     required this.keyboardType,
+//     required this.labelText,
+//     required this.controller,
+//     this.validator,
+//     this.onSaved,
+//     this.onChanged,
+//   }) : super(key: key);
+//
+//   @override
+//   State<LoginPhoneTextField> createState() => _LoginPhoneTextFieldState();
+// }
+//
+// class _LoginPhoneTextFieldState extends State<LoginPhoneTextField> {
+//   final countryPicker = const FlCountryCodePicker();
+//
+//   InputDecoration buildInputDecoration() {
+//     return InputDecoration(
+//         border: InputBorder.none,
+//         contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+//         filled: true,
+//         fillColor: Colors.white,
+//         enabledBorder: OutlineInputBorder(
+//           borderSide: BorderSide.none,
+//           borderRadius: BorderRadius.circular(10),
+//         ),
+//         focusedBorder: OutlineInputBorder(
+//           borderSide: BorderSide.none,
+//           borderRadius: BorderRadius.circular(10),
+//         ),
+//         errorBorder: OutlineInputBorder(
+//           borderSide: BorderSide.none,
+//           borderRadius: BorderRadius.circular(10),
+//         ),
+//         focusedErrorBorder: OutlineInputBorder(
+//           borderSide: BorderSide.none,
+//           borderRadius: BorderRadius.circular(10),
+//         ),
+//         prefixIcon: Container(
+//           padding: EdgeInsets.symmetric(horizontal: 0, vertical: 6),
+//           margin: EdgeInsets.symmetric(horizontal: 0),
+//           child: Row(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 6),
+//                 child: Row(
+//                   children: [
+//                     Text(
+//                       publiccountryCode?.dialCode ?? "+91",
+//                       style: TextStyle(
+//                         color: Colors.black,
+//                         fontSize: 16,
+//                         fontWeight: FontWeight.w300,
+//                         letterSpacing: -0.18,
+//                       ),
+//                     ),
+//                     GestureDetector(
+//                         onTap: () async {
+//                           final code =
+//                               await countryPicker.showPicker(context: context);
+//                           setState(() {
+//                             publiccountryCode = code;
+//                           });
+//                         },
+//                         child: Icon(
+//                           Icons.keyboard_arrow_down,
+//                           color: SColors.color3,
+//                           size: 28,
+//                         )),
+//
+//                     // const SizedBox(width: 5),
+//                     VerticalDivider(
+//                       thickness: 2,
+//                       color: Colors.grey.withOpacity(0.4),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ));
+//   }
+//
+//
+//   Widget textField(TextEditingController controller) {
+//     return GestureDetector(
+//       child: Container(
+//         height: 35,
+//         decoration: BoxDecoration(
+//             color: Colors.white, borderRadius: BorderRadius.circular(8)),
+//         child: TextFormField(
+//           maxLines: 1,
+//           onChanged: widget.onChanged,
+//           onSaved: widget.onSaved,
+//           validator: widget.validator,
+//           controller: controller,
+//           keyboardType: widget.keyboardType,
+//           cursorColor: Colors.black,
+//           style: TextStyle(
+//             color: SColors.color3,
+//             fontSize: 16,
+//             fontWeight: FontWeight.w400,
+//           ),
+//           autocorrect: true,
+//           decoration: buildInputDecoration(),
+//         ),
+//
+//       ),
+//
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 18),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Text(
+//             widget.labelText,
+//             style: TextStyle(
+//               color: SColors.color3,
+//               fontSize: 11,
+//               fontWeight: FontWeight.w400,
+//             ),
+//           ),
+//           const SizedBox(height: 8),
+//           textField(widget.controller),
+//           const SizedBox(height: 25),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+//
 import 'package:flutter/material.dart';
+import 'package:fl_country_code_picker/fl_country_code_picker.dart';
 import 'package:get/get.dart';
 import 'package:stellar_chat/Settings/SColors.dart';
 
@@ -29,126 +183,148 @@ class LoginPhoneTextField extends StatefulWidget {
 
 class _LoginPhoneTextFieldState extends State<LoginPhoneTextField> {
   final countryPicker = const FlCountryCodePicker();
+  double containerHeight = 35;
 
-  InputDecoration buildInputDecoration() {
-    return InputDecoration(
-        border: InputBorder.none,
-        contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-        filled: true,
-        fillColor: Colors.white,
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        prefixIcon: Container(
-          padding: EdgeInsets.symmetric(horizontal: 0, vertical: 6),
-          margin: EdgeInsets.symmetric(horizontal: 0),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 6),
-                child: Row(
-                  children: [
-                    Text(
-                      publiccountryCode?.dialCode ?? "+91",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w300,
-                        letterSpacing: -0.18,
-                      ),
-                    ),
-                    GestureDetector(
-                        onTap: () async {
-                          final code =
-                              await countryPicker.showPicker(context: context);
-                          setState(() {
-                            publiccountryCode = code;
-                          });
-                        },
-                        child: Icon(
-                          Icons.keyboard_arrow_down,
-                          color: SColors.color3,
-                          size: 28,
-                        )),
-
-                    // const SizedBox(width: 5),
-                    VerticalDivider(
-                      thickness: 2,
-                      color: Colors.grey.withOpacity(0.4),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ));
+  void _increaseContainerHeight() {
+    setState(() {
+      containerHeight = 50;
+    });
   }
 
+  void _resetContainerHeight() {
+    setState(() {
+      containerHeight = 35;
+    });
+  }
+
+  InputDecoration buildInputDecoration({double contentVerticalPadding = 15}) {
+    return InputDecoration(
+      border: InputBorder.none,
+      contentPadding: EdgeInsets.symmetric(vertical: contentVerticalPadding, horizontal: 15),
+      filled: true,
+      fillColor: Colors.white,
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      prefixIcon: Container(
+        padding: EdgeInsets.symmetric(horizontal: 0, vertical: 6),
+        margin: EdgeInsets.symmetric(horizontal: 0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 6),
+              child: Row(
+                children: [
+                  Text(
+                    publiccountryCode?.dialCode ?? "+91",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      letterSpacing: -0.18,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      final code = await countryPicker.showPicker(context: context);
+                      setState(() {
+                        publiccountryCode = code;
+                      });
+                    },
+                    child: Icon(Icons.keyboard_arrow_down, color: SColors.color3, size: 28,),
+                  ),
+                  containerHeight == 50 ? const Padding(
+                    padding: EdgeInsets.all(7),
+                    child: VerticalDivider(thickness: 1, color: Colors.grey,),
+                  ) : const VerticalDivider(thickness: 1, color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   Widget textField(TextEditingController controller) {
     return GestureDetector(
       child: Container(
-        height: 35,
+        height: containerHeight,
         decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(8)),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
         child: TextFormField(
           maxLines: 1,
           onChanged: widget.onChanged,
           onSaved: widget.onSaved,
-          validator: widget.validator,
+          validator: (val) {
+            final error = widget.validator!(val);
+            if (error != null) {
+              _increaseContainerHeight();
+            } else {
+              _resetContainerHeight();
+            }
+            return error;
+          },
           controller: controller,
           keyboardType: widget.keyboardType,
           cursorColor: Colors.black,
           style: TextStyle(
-            color: SColors.color3,
+            color:SColors.color3,
             fontSize: 16,
             fontWeight: FontWeight.w400,
           ),
           autocorrect: true,
-          decoration: buildInputDecoration(),
+          decoration: buildInputDecoration(
+            contentVerticalPadding: containerHeight == 50 ? 11 : 0,
+          ),
         ),
-
       ),
-
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            widget.labelText,
-            style: TextStyle(
-              color: SColors.color3,
-              fontSize: 11,
-              fontWeight: FontWeight.w400,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.labelText,
+              style: TextStyle(
+                color: SColors.color3,
+                fontSize: 11,
+                fontWeight: FontWeight.w400,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          textField(widget.controller),
-          const SizedBox(height: 25),
-        ],
+            const SizedBox(height: 8),
+            textField(widget.controller),
+            const SizedBox(height: 25),
+          ],
+        ),
       ),
     );
   }
 }
+
 
 
