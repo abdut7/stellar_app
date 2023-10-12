d053040285bf883dcfe10c6dbe51e1c9348c1a15
Nithin-M-007 <isnithinm2001@gmail.com>
2023-10-12 16:25:38 +0530

    some changes

--- lib/View/discover/discover_homescreen.dart
+++ lib/View/discover/discover_homescreen.dart
@@ -8,6 +8,7 @@ import 'package:stellar_chat/View/channel/channel_home_screen/channel_home_scree
 import 'package:stellar_chat/View/contact/add_contact/friends_in_radar/friends_in_radar.dart';
 import 'package:stellar_chat/View/discover/widgets/discover_list_tile.dart';
 import 'package:stellar_chat/View/discover/widgets/top_section.dart';
+import 'package:stellar_chat/stellar_pay/screens/stellar_pay_splash_screen/stellar_pay_splash.dart';
 import 'package:stellar_chat/View/flicks/flicks_player_home_screen.dart';
 
 class DiscoverHomeScreen extends StatefulWidget {
