fe60a8c162233b73cd825b820617e55df76fc26b
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-10-17 16:21:43 +0530

    added comment time issue

--- lib/View/comment_view/functions/get_time_as_ago.dart
+++ lib/View/comment_view/functions/get_time_as_ago.dart
@@ -1,29 +1,22 @@
 import 'package:intl/intl.dart';
 
 String formatTimeDifference(String timestamp) {
-  // DateTime currentTime = DateTime.now();
-  // DateTime parsedTime = DateTime.parse(timestamp);
-  // Duration difference = currentTime.difference(parsedTime);
+  DateTime currentTime = DateTime.now();
+  DateTime time = DateTime.parse(timestamp);
 
-  // if (difference.inSeconds < 0) {
-  //   return 'now';
-  // }
+  Duration difference = time.difference(currentTime);
 
-  // if (difference.inSeconds < 60) {
-  //   return '${difference.inSeconds} seconds ago';
-  // } else if (difference.inMinutes < 60) {
-  //   return '${difference.inMinutes} minutes ago';
-  // } else if (difference.inHours < 24) {
-  //   return '${difference.inHours} hours ago';
-  // } else if (difference.inDays < 30) {
-  //   return '${difference.inDays} days ago';
-  // } else if (difference.inDays < 365) {
-  //   final months = difference.inDays ~/ 30;
-  //   return '${months} ${months == 1 ? "month" : "months"} ago';
-  // } else {
-  //   final years = difference.inDays ~/ 365;
-  //   return '${years} ${years == 1 ? "year" : "years"} ago';
-  // }
-
-  return "now";
+  if (difference.inSeconds < 60) {
+    return "${difference.inSeconds} seconds ago";
+  } else if (difference.inMinutes < 60) {
+    return "${difference.inMinutes} minutes ago";
+  } else if (difference.inHours < 24) {
+    return "${difference.inHours} hours ago";
+  } else if (difference.inDays < 365) {
+    final dateFormat = DateFormat("MMMM d");
+    return dateFormat.format(time);
+  } else {
+    final dateFormat = DateFormat("yMMMMd");
+    return dateFormat.format(time);
+  }
 }
