fdffe3ff31c450e5fac482c43adc8c598c92fff5
Nithin-M-007 <isnithinm2001@gmail.com>
2023-10-18 09:42:32 +0530

    add contact screen enabled dark mode

--- lib/widgets/add_contact_listtile.dart
+++ lib/widgets/add_contact_listtile.dart
@@ -1,6 +1,8 @@
+import 'package:get/get.dart';
 import 'package:stellar_chat/Settings/SColors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:stellar_chat/controllers/theme_controller.dart';
 
 class AddContactListTile extends StatefulWidget {
   final String text;
@@ -22,6 +24,7 @@ class AddContactListTile extends StatefulWidget {
 class _AddContactListTileState extends State<AddContactListTile> {
   @override
   Widget build(BuildContext context) {
+    ThemeController themeController = Get.find();
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: ListTile(
@@ -34,20 +37,30 @@ class _AddContactListTileState extends State<AddContactListTile> {
         title: Text(
           widget.text,
           style: TextStyle(
-            color: SColors.color3,
+            color:  themeController.isDarkTheme.value
+                ?  SColors.color4
+                : SColors.color3,
             fontSize: 14,
             fontWeight: FontWeight.w900,
           ),
         ),
         subtitle: Text(
           widget.subtitle,
-          style: TextStyle(fontSize: 11, color: SColors.color3,fontWeight: FontWeight.w600),
+          style: TextStyle(
+              fontSize: 11,
+              color:  themeController.isDarkTheme.value
+                  ?  SColors.buttonTextIndark
+                  : SColors.color3,
+              fontWeight: FontWeight.w600
+          ),
         ),
         trailing: IconButton(
           icon: Icon(
             Icons.arrow_forward_ios,
             size: 18,
-            color: SColors.color3,
+            color:  themeController.isDarkTheme.value
+                ?  SColors.color4
+                : SColors.color3,
           ),
           onPressed: widget.onPressed,
         ),
