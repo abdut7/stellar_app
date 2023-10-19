571ace6272713a6ebe4eb167d8bcfa007548e10f
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-10-19 13:46:18 +0530

    Small UI and comment issue fixed

--- lib/View/comment_view/functions/get_time_as_ago.dart
+++ lib/View/comment_view/functions/get_time_as_ago.dart
@@ -4,14 +4,18 @@ String formatTimeDifference(String timestamp) {
   DateTime currentTime = DateTime.now();
   DateTime time = DateTime.parse(timestamp);
 
-  Duration difference = time.difference(currentTime);
+  Duration difference = currentTime.difference(time);
+
+  if (difference.inSeconds < 0) {
+    return "Now";
+  }
 
   if (difference.inSeconds < 60) {
-    return "${difference.inSeconds} seconds ago";
+    return "${difference.inSeconds} sec ago";
   } else if (difference.inMinutes < 60) {
-    return "${difference.inMinutes} minutes ago";
+    return "${difference.inMinutes} min ago";
   } else if (difference.inHours < 24) {
-    return "${difference.inHours} hours ago";
+    return "${difference.inHours} hr ago";
   } else if (difference.inDays < 365) {
     final dateFormat = DateFormat("MMMM d");
     return dateFormat.format(time);
